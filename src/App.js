1ec4358bf26d1601e5101a003445e4bb03ed5001
Siddharth Venkatesh <32375112+sidv93@users.noreply.github.com>
2020-07-15 02:16:27 +0530

    added basic layout and some animations

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import LeftArrow from './assets/arrow-left.svg';
+import RightArrow from './assets/arrow-right.svg';
+import Testimonial from './Testimonial';
+
+const testimonials = [
+  {
+    name: "Julia Cameron",
+    title: "Creative Director, VISA",
+    image: `${require("./assets/image3.jpg")}`,
+    quote:
+      "It's all good. I was amazed at the quality of the Design. We've seen amazing results already."
+  },
+  {
+    name: "Mark Jacobs",
+    title: "Tech Lead, Google",
+    image: `${require("./assets/image1.jpg")}`,
+    quote:
+      "The rebranding has really helped our business. Definitely worth the investment."
+  },
+  {
+    name: "Lisa Bearings",
+    title: "Brand Coordinator, Facebook",
+    image: `${require("./assets/image2.jpg")}`,
+    quote:
+      "The service was excellent. Absolutely wonderful! A complete redesign did it for us."
+  }
+];
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+
+  &:after {
+    content: "";
+    position: absolute;
+    width: 700px;
+    height: 420px;
+    background: #f2f2f6;
+    right: 0;
+    bottom: 0;
+    opacity: 0.8;
+    z-index: -10;
+  }
+`;
+const TestimonialContainer = styled.div`
+  height: 50vh;
+  width: 60vw;
+  display: flex;
+`;
+const ArrowContainer = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+
+  &:hover {
+    box-shadow: 0px 0px 30px rgba(0, 0, 80, 0.05);
+  }
+`;
+const Img = styled.img`
+  height: 12px;
+  width: 20px;
+  flex: 1;
+`;
 
 function App() {
+  const [active, setActive] = useState(testimonials[0]);
+  const [current, setCurrent] = useState(0);
+  setTimeout(() => {
+    if(current < 2) {
+      setCurrent(current+1);
+      setActive(testimonials[current+1]);
+    } else {
+      setCurrent(0);
+      setActive(testimonials[0]);
+    }
+  }, 4000)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <TestimonialContainer>
+        <ArrowContainer>
+          <Img src={LeftArrow} alt="left arrow" />
+        </ArrowContainer>
+        <Testimonial testimonial={active} />
+        <ArrowContainer>
+          <Img src={RightArrow} alt="right arrow" />
+        </ArrowContainer>
+      </TestimonialContainer>
+    </Container>
   );
 }
 
---
+++ src/Testimonial.js
@@ -0,0 +1,103 @@
+import React, { useEffect } from 'react';
+import styled from 'styled-components';
+import { motion, AnimatePresence } from 'framer-motion';
+
+const transition = { duration: 2, ease: [0.6, 0.01, -0.05, 0.9] };
+
+const Container = styled.div`
+    flex: 7;
+    padding: 20px 30px;
+    display: flex;
+`;
+const ImageContainer = styled.div`
+    flex: 2;
+    position: relative;
+
+    &:after {
+        content: '';
+        position: absolute;
+        width: 200px;
+        height: 200px;
+        background: #3f56da;
+        left: -100px;
+        top: -70px;
+        border-radius: 100%;
+        z-index: -9;
+    }
+`;
+const Image = styled(motion.img)`
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+`;
+const ContentContainer = styled(motion.div)`
+    flex: 3;
+    padding: 0 30px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+`;
+const Quote = styled.h3`
+    color: #2e293c;
+    letter-spacing: 1px;
+    line-height: 30px;
+`;
+const Name = styled.h5`
+    color: #a09da6;
+    margin: 15px 0;
+`;
+const Title = styled.h6`
+    color: #a09da6;
+    margin: 5px 0;
+`;
+
+const Testimonial = ({ testimonial }) => {
+    return (
+        <AnimatePresence>
+            <Container>
+                <ImageContainer>
+                    <span style={{ overflow: 'hidden', width: '100%', height: '100%' }}>
+                        <AnimatePresence>
+                            <Image
+                                key={testimonial.image}
+                                initial={{
+                                    x: 100,
+                                    scale: 1.2,
+                                    opacity: 0
+                                }}
+                                animate={{
+                                    x: 0,
+                                    scale: 1,
+                                    opacity: 1
+                                }}
+                                exit={{
+                                    x: -100,
+                                    opacity: 0
+                                }}
+                                transition={transition}
+                                src={testimonial.image} alt="image of person" />
+                        </AnimatePresence>
+                    </span>
+                </ImageContainer>
+                <ContentContainer
+                    initial={{
+                        opacity: 0
+                    }}
+                    animate={{
+                        opacity: 1
+                    }}
+                    exit={{
+                        opacity: 0
+                    }}
+                    transition={transition}
+                >
+                    <Quote>{testimonial.quote}</Quote>
+                    <Name>{testimonial.name}</Name>
+                    <Title>{testimonial.title}</Title>
+                </ContentContainer>
+            </Container>
+        </AnimatePresence>
+    );
+};
+
+export default Testimonial;
